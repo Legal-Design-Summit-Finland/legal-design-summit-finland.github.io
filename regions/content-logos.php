30788c01976426718c42a4a6f8250cdcb4437265
Antti Toivonen <ast@kranken.dyndns.org>
2019-05-29 12:47:23 +0300

    Made possible cleanup

--- regions/content-logos.php
+++ regions/content-logos.php
@@ -14,18 +14,9 @@
 		<h3>Made possible by</h3>
 
 		<div class="row padding-top-20">
-			<div class="col-md-2 col-md-offset-2 justify-center"><div class="logo"><a href="https://www.varma.fi" target="_blank"><img src="images/logos/logo_varma.png?<?php print time(); ?>"></a></div></div>
-			<div class="col-md-2 justify-center"><div class="logo"><a href="http://suomifinland100.fi" target="_blank"><img src="images/logos/logo_suomi-finland-100.png?<?php print time(); ?>"></a></div></div>
-			<div class="col-md-2 justify-center"><div class="logo"><a href="http://jandersdean.com" target="_blank"><img src="images/logos/logo_janders-dean.png?<?php print time(); ?>"></a></div></div>
 			<div class="col-md-2 justify-center"><div class="logo"><a href="http://www.helsinki.fi/oikeustiede" target="_blank"><img src="images/logos/logo_oikeustieteellinen-tiedekunta.png?<?php print time(); ?>"></a></div></div>
+			<div class="col-md-2 justify-center"><div class="logo"><a href="http://www.legaltechlab.fi" target="_blank"><img src="images/logos/logo_legal-tech-lab.png?<?php print time(); ?>"></a></div></div>
 		</div>
-		
-		<div class="row">
-			<div class="col-md-2 col-md-offset-3 justify-center"><div class="logo"><a href="http://www.legaltechlab.fi" target="_blank"><img src="images/logos/logo_legal-tech-lab.png?<?php print time(); ?>"></a></div></div>
-			<div class="col-md-2 justify-center"><div class="logo"><a href="http://www.klok.fi" target="_blank"><img src="images/logos/logo_klok.png?<?php print time(); ?>"></a></div></div>
-			<div class="col-md-2 justify-center"><div class="logo"><a href="https://www.almatalent.fi" target="_blank"><img src="images/logos/logo_alma-talent.png?<?php print time(); ?>"></a></div></div>
-		</div>
-		
 	</div>
 	
 	<div class="padding-top-40"></div>
