0e15a80a0102c989806b1397c8f6f760e39efc4d
Antti Toivonen <ast@kranken.dyndns.org>
2019-05-29 12:54:01 +0300

    Logo cleanup

--- regions/content-logos.php
+++ regions/content-logos.php
@@ -1,24 +1,5 @@
 <div class="logos justify-center">
 	
-	<h3>Created by</h3>
-	
-	<div class="row padding-top-20">
-		<div class="col-md-2 col-md-offset-4 justify-center"><div class="logo"><a href="http://www.hellon.com" target="_blank"><img src="images/logos/logo_hellon.png?<?php print time(); ?>"></a></div></div>
-		<div class="col-md-2 justify-center"><div class="logo"><a href="http://dottirlaw.com" target="_blank"><img src="images/logos/logo_dottir.png?<?php print time(); ?>"></a></div></div>
-	</div>
-	
-	<div class="padding-top-40"></div>
-	
-	<div class="padding-top-40">
-
-		<h3>Made possible by</h3>
-
-		<div class="row padding-top-20">
-			<div class="col-md-2 justify-center"><div class="logo"><a href="http://www.helsinki.fi/oikeustiede" target="_blank"><img src="images/logos/logo_oikeustieteellinen-tiedekunta.png?<?php print time(); ?>"></a></div></div>
-			<div class="col-md-2 justify-center"><div class="logo"><a href="http://www.legaltechlab.fi" target="_blank"><img src="images/logos/logo_legal-tech-lab.png?<?php print time(); ?>"></a></div></div>
-		</div>
-	</div>
-	
 	<div class="padding-top-40"></div>
 	
 	<div class="padding-top-40">
@@ -26,21 +7,12 @@
 		<h3>In co-operation with</h3>
 
 		<div class="row padding-top-20">
-			<div class="col-md-2 justify-center"><div class="logo"><a href="https://www.lakimiesliitto.fi" target="_blank"><img src="images/logos/logo_suomen-lakimiesliitto.png?<?php print time(); ?>"></a></div></div>
+                        <div class="col-md-2 justify-center"><div class="logo"><a href="http://www.helsinki.fi/oikeustiede" target="_blank"><img src="images/logos/logo_oikeustieteellinen-tiedekunta.png?<?php print time(); ?>"></a></div></div>
+                        <div class="col-md-2 justify-center"><div class="logo"><a href="http://www.legaltechlab.fi" target="_blank"><img src="images/logos/logo_legal-tech-lab.png?<?php print time(); ?>"></a></div></div>
 			<div class="col-md-2 justify-center"><div class="logo"><a href="https://itpaalto.net" target="_blank"><img src="images/logos/logo_aalto-itp.png?<?php print time(); ?>"></a></div></div>
-			<div class="col-md-2 justify-center"><div class="logo"><a href="https://legalhackers.org" target="_blank"><img src="images/logos/logo_legal-hackers.png?<?php print time(); ?>"></a></div></div>
-			<div class="col-md-2 justify-center"><div class="logo"><a href="http://www.ornamo.fi" target="_blank"><img src="images/logos/logo_ornamo.png?<?php print time(); ?>"></a></div></div>
 			<div class="col-md-2 justify-center"><div class="logo"><a href="http://www.ulapland.fi" target="_blank"><img src="images/logos/logo_lapin-yliopisto.png?<?php print time(); ?>"></a></div></div>
 			<div class="col-md-2 justify-center"><div class="logo"><a href="http://www.utu.fi" target="_blank"><img src="images/logos/logo_turun-yliopisto.png?<?php print time(); ?>"></a></div></div>
-		</div>
-		
-		<div class="row">
 			<div class="col-md-2 justify-center"><div class="logo"><a href="http://www.uef.fi" target="_blank"><img src="images/logos/logo_ita-suomen-yliopisto.png?<?php print time(); ?>"></a></div></div>
-			<div class="col-md-2 justify-center"><div class="logo"><a href="http://www.houthoff.com" target="_blank"><img src="images/logos/logo_houthoff-buruma.png?<?php print time(); ?>"></a></div></div>
-			<div class="col-md-2 justify-center"><div class="logo"><a href="https://www.dentons.com" target="_blank"><img src="images/logos/logo_dentons.png?<?php print time(); ?>"></a></div></div>
-			<div class="col-md-2 justify-center"><div class="logo"><a href="http://oikeusministerio.fi/" target="_blank"><img src="images/logos/logo_oikeusministerio.png?<?php print time(); ?>"></a></div></div>
-			<div class="col-md-2 justify-center"><div class="logo"><a href="https://oikeus.fi/fi/index/ajankohtaista/aipa-hanke.html" target="_blank"><img src="images/logos/logo_aipa.png?<?php print time(); ?>"></a></div></div>
-			<div class="col-md-2 justify-center"><div class="logo"><a href="http://legal-tech-association.eu" target="_blank"><img src="images/logos/logo_elta.png?<?php print time(); ?>"></a></div></div>
 		</div>
 
 	</div>
