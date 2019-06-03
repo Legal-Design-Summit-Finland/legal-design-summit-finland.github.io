1e9932419a9727981b240fa9245fa86ddef596b4
Antti Toivonen <ast@kranken.dyndns.org>
2019-06-03 23:18:18 +0300

    Side events and header tuning

--- pages/side-events.php
+++ pages/side-events.php
@@ -7,6 +7,7 @@
         <div id="content-side-events" class="justify-center">
 
 		<h2>Side Events</h2>
+		<h4>11-12.9.2019</h4>
 		<p>The side events are related events organized by our partners. They are a unique chance to learn more about more specific legal design subject, hear case examples and to network.</p>
 		<p>The side events will be announced and posted later and you can register to the side events through the links on on this page.</p>
 		<p><strong>Are you interested to organize a side event? Contact us!</strong> <a href="mailto:&#105;&#110;&#102;&#111;&#064;&#108;&#101;&#103;&#097;&#108;&#100;&#101;&#115;&#105;&#103;&#110;&#115;&#117;&#109;&#109;&#105;&#116;&#046;&#099;&#111;&#109;">&#105;&#110;&#102;&#111;&#064;&#108;&#101;&#103;&#097;&#108;&#100;&#101;&#115;&#105;&#103;&#110;&#115;&#117;&#109;&#109;&#105;&#116;&#046;&#099;&#111;&#109;</a></p>
