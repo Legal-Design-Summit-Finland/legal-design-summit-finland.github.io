a30a291f100e1e50026bee397de834210f2e76b2
Antti Toivonen <ast@kranken.dyndns.org>
2019-06-03 18:39:31 +0300

    Removed Aalto logo. Removed Dottir and Hellon from footer.

--- regions/content-logos.php
+++ regions/content-logos.php
@@ -9,7 +9,6 @@
 		<div class="row padding-top-20">
                         <div class="col-md-2 justify-center"><div class="logo"><a href="http://www.helsinki.fi/oikeustiede" target="_blank"><img src="images/logos/logo_oikeustieteellinen-tiedekunta.png?<?php print time(); ?>"></a></div></div>
                         <div class="col-md-2 justify-center"><div class="logo"><a href="http://www.legaltechlab.fi" target="_blank"><img src="images/logos/logo_legal-tech-lab.png?<?php print time(); ?>"></a></div></div>
-			<div class="col-md-2 justify-center"><div class="logo"><a href="https://itpaalto.net" target="_blank"><img src="images/logos/logo_aalto-itp.png?<?php print time(); ?>"></a></div></div>
 			<div class="col-md-2 justify-center"><div class="logo"><a href="http://www.ulapland.fi" target="_blank"><img src="images/logos/logo_lapin-yliopisto.png?<?php print time(); ?>"></a></div></div>
 			<div class="col-md-2 justify-center"><div class="logo"><a href="http://www.utu.fi" target="_blank"><img src="images/logos/logo_turun-yliopisto.png?<?php print time(); ?>"></a></div></div>
 			<div class="col-md-2 justify-center"><div class="logo"><a href="http://www.uef.fi" target="_blank"><img src="images/logos/logo_ita-suomen-yliopisto.png?<?php print time(); ?>"></a></div></div>
--- regions/footer.php
+++ regions/footer.php
@@ -2,6 +2,6 @@
 
 	<p><strong>Contact:</strong><br><a href="mailto:&#105;&#110;&#102;&#111;&#064;&#108;&#101;&#103;&#097;&#108;&#100;&#101;&#115;&#105;&#103;&#110;&#115;&#117;&#109;&#109;&#105;&#116;&#046;&#099;&#111;&#109;">&#105;&#110;&#102;&#111;&#064;&#108;&#101;&#103;&#097;&#108;&#100;&#101;&#115;&#105;&#103;&#110;&#115;&#117;&#109;&#109;&#105;&#116;&#046;&#099;&#111;&#109;</a></p>
 	
-	<p class="small">Legal Design Summit reserves the right to make changes to the programme and the speakers. Legal Design Summit is organized by Hellon Ltd in cooperation with Dottir Attorneys Ltd.</p>
+	<p class="small">Legal Design Summit reserves the right to make changes to the programme and the speakers.</p>
 	
 </div>
