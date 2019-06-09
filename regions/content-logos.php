79037db2d3e65a8c4fa06cff45cfeb642bebfb2c
Antti Toivonen <ast@kranken.dyndns.org>
2019-06-09 22:02:44 +0300

    Added Helsinki, MLTech and ELTA logos.

--- regions/content-logos.php
+++ regions/content-logos.php
@@ -1,5 +1,14 @@
 <div class="logos justify-center">
-	
+       <div class="padding-top-40">
+
+               <h3>Made possible by</h3>
+
+               <div class="row padding-top-20">
+                       <div class="col-md-2-offset-2 justify-center"><div class="logo"><a href="https://www.hel.fi" target="_blank"><img src="images/logos/logo_helsinki.jpg?<?php print time(); ?>"></a></div></div>
+               </div>
+
+       </div>
+
 	<div class="padding-top-40"></div>
 	
 	<div class="padding-top-40">
@@ -7,12 +16,17 @@
 		<h3>In co-operation with</h3>
 
 		<div class="row padding-top-20">
-                        <div class="col-md-2 justify-center"><div class="logo"><a href="http://www.helsinki.fi/oikeustiede" target="_blank"><img src="images/logos/logo_oikeustieteellinen-tiedekunta.png?<?php print time(); ?>"></a></div></div>
+                        <div class="col-md-2 col-md-offset-2 justify-center"><div class="logo"><a href="http://www.helsinki.fi/oikeustiede" target="_blank"><img src="images/logos/logo_oikeustieteellinen-tiedekunta.png?<?php print time(); ?>"></a></div></div>
                         <div class="col-md-2 justify-center"><div class="logo"><a href="http://www.legaltechlab.fi" target="_blank"><img src="images/logos/logo_legal-tech-lab.png?<?php print time(); ?>"></a></div></div>
 			<div class="col-md-2 justify-center"><div class="logo"><a href="http://www.ulapland.fi" target="_blank"><img src="images/logos/logo_lapin-yliopisto.png?<?php print time(); ?>"></a></div></div>
 			<div class="col-md-2 justify-center"><div class="logo"><a href="http://www.utu.fi" target="_blank"><img src="images/logos/logo_turun-yliopisto.png?<?php print time(); ?>"></a></div></div>
-			<div class="col-md-2 justify-center"><div class="logo"><a href="http://www.uef.fi" target="_blank"><img src="images/logos/logo_ita-suomen-yliopisto.png?<?php print time(); ?>"></a></div></div>
-			<div class="col-md-2 justify-center"><div class="logo"><a href="https://www.laurea.fi/koulutus/liiketalous-ja-tietojenkasittely/oikeudellinen-erityisosaaminen-ja-oikeusmuotoilu/" target="_blank"><img src="images/logos/logo_laurea.png?<?php print time(); ?>"></a></div></div>
+		</div>
+
+		<div class="row">
+			<div class="col-md-2 col-md-offset-2 justify-center"><div class="logo"><a href="http://www.uef.fi" target="_blank"><img src="images/logos/logo_ita-suomen-yliopisto.png?<?php print time(); ?>"></a></div></div>
+                        <div class="col-md-2 justify-center"><div class="logo"><a href="https://www.laurea.fi/koulutus/liiketalous-ja-tietojenkasittely/oikeudellinen-erityisosaaminen-ja-oikeusmuotoilu/" target="_blank"><img src="images/logos/logo_laurea.png?<?php print time(); ?>"></a></div></div>
+			<div class="col-md-2 justify-center"><div class="logo"><a href="https://www.ml-tech.org" target="_blank"><img src="images/logos/logo_mltech.jpg?<?php print time(); ?>"></a></div></div>
+			<div class="col-md-2 justify-center"><div class="logo"><a href="https://europe-legaltech.org" target="_blank"><img src="images/logos/logo_elta.png?<?php print time(); ?>"></a></div></div>
 		</div>
 
 	</div>
