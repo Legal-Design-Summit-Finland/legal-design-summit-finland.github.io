135f790ed9c172b414da232654385cc7ca17524c
Antti Toivonen <ast@kranken.dyndns.org>
2019-06-09 22:35:19 +0300

    Added privacy policy

--- regions/footer.php
+++ regions/footer.php
@@ -3,5 +3,6 @@
 	<p><strong>Contact:</strong><br><a href="mailto:&#105;&#110;&#102;&#111;&#064;&#108;&#101;&#103;&#097;&#108;&#100;&#101;&#115;&#105;&#103;&#110;&#115;&#117;&#109;&#109;&#105;&#116;&#046;&#099;&#111;&#109;">&#105;&#110;&#102;&#111;&#064;&#108;&#101;&#103;&#097;&#108;&#100;&#101;&#115;&#105;&#103;&#110;&#115;&#117;&#109;&#109;&#105;&#116;&#046;&#099;&#111;&#109;</a></p>
 	
 	<p class="small">Legal Design Summit reserves the right to make changes to the programme and the speakers.</p>
+	<p><a href="/privacy#section-privacy">Privacy Policy</a></p>
 	
 </div>
