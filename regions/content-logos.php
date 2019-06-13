4cf44bab466f4d861235bf7081d5d0c7f28ea639
Antti Toivonen <ast@kranken.dyndns.org>
2019-06-13 22:41:55 +0300

    Reordered the logos

--- regions/content-logos.php
+++ regions/content-logos.php
@@ -16,10 +16,10 @@
 		<h3>In co-operation with</h3>
 
 		<div class="row padding-top-20">
-                        <div class="col-md-2 col-md-offset-2 justify-center"><div class="logo"><a href="http://www.helsinki.fi/oikeustiede" target="_blank"><img src="images/logos/logo_oikeustieteellinen-tiedekunta.png?<?php print time(); ?>"></a></div></div>
+			<div class="col-md-2 col-md-offset-2 justify-center"><div class="logo"><a href="https://law.stanford.edu/organizations/pages/legal-design-lab/" target="_blank"><img src="images/logos/logo_stanford.png?<?php print time(); ?>"></a></div></div>
                         <div class="col-md-2 justify-center"><div class="logo"><a href="http://www.legaltechlab.fi" target="_blank"><img src="images/logos/logo_legal-tech-lab.png?<?php print time(); ?>"></a></div></div>
+                        <div class="col-md-2 justify-center"><div class="logo"><a href="http://www.helsinki.fi/oikeustiede" target="_blank"><img src="images/logos/logo_oikeustieteellinen-tiedekunta.png?<?php print time(); ?>"></a></div></div>
 			<div class="col-md-2 justify-center"><div class="logo"><a href="http://www.ulapland.fi" target="_blank"><img src="images/logos/logo_lapin-yliopisto.png?<?php print time(); ?>"></a></div></div>
-			<div class="col-md-2 justify-center"><div class="logo"><a href="https://law.stanford.edu/organizations/pages/legal-design-lab/" target="_blank"><img src="images/logos/logo_stanford.png?<?php print time(); ?>"></a></div></div>
 		</div>
 
 		<div class="row">
