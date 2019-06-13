8aa0a5b0edca71807272e7470e5101308e08d2d4
Antti Toivonen <ast@kranken.dyndns.org>
2019-06-13 22:30:29 +0300

    Added Stanford and Legal Hackers logos

--- regions/content-logos.php
+++ regions/content-logos.php
@@ -19,15 +19,19 @@
                         <div class="col-md-2 col-md-offset-2 justify-center"><div class="logo"><a href="http://www.helsinki.fi/oikeustiede" target="_blank"><img src="images/logos/logo_oikeustieteellinen-tiedekunta.png?<?php print time(); ?>"></a></div></div>
                         <div class="col-md-2 justify-center"><div class="logo"><a href="http://www.legaltechlab.fi" target="_blank"><img src="images/logos/logo_legal-tech-lab.png?<?php print time(); ?>"></a></div></div>
 			<div class="col-md-2 justify-center"><div class="logo"><a href="http://www.ulapland.fi" target="_blank"><img src="images/logos/logo_lapin-yliopisto.png?<?php print time(); ?>"></a></div></div>
-			<div class="col-md-2 justify-center"><div class="logo"><a href="http://www.utu.fi" target="_blank"><img src="images/logos/logo_turun-yliopisto.png?<?php print time(); ?>"></a></div></div>
+			<div class="col-md-2 justify-center"><div class="logo"><a href="https://law.stanford.edu/organizations/pages/legal-design-lab/" target="_blank"><img src="images/logos/logo_stanford.png?<?php print time(); ?>"></a></div></div>
 		</div>
 
 		<div class="row">
 			<div class="col-md-2 col-md-offset-2 justify-center"><div class="logo"><a href="http://www.uef.fi" target="_blank"><img src="images/logos/logo_ita-suomen-yliopisto.png?<?php print time(); ?>"></a></div></div>
+			<div class="col-md-2 justify-center"><div class="logo"><a href="http://www.utu.fi" target="_blank"><img src="images/logos/logo_turun-yliopisto.png?<?php print time(); ?>"></a></div></div>
                         <div class="col-md-2 justify-center"><div class="logo"><a href="https://www.laurea.fi/koulutus/liiketalous-ja-tietojenkasittely/oikeudellinen-erityisosaaminen-ja-oikeusmuotoilu/" target="_blank"><img src="images/logos/logo_laurea.png?<?php print time(); ?>"></a></div></div>
 			<div class="col-md-2 justify-center"><div class="logo"><a href="https://www.ml-tech.org" target="_blank"><img src="images/logos/logo_mltech.jpg?<?php print time(); ?>"></a></div></div>
-			<div class="col-md-2 justify-center"><div class="logo"><a href="https://europe-legaltech.org" target="_blank"><img src="images/logos/logo_elta.png?<?php print time(); ?>"></a></div></div>
 		</div>
+                <div class="row">
+			<div class="col-md-2 col-md-offset-4 justify-center"><div class="logo"><a href="https://europe-legaltech.org" target="_blank"><img src="images/logos/logo_elta.png?<?php print time(); ?>"></a></div></div>
+                        <div class="col-md-2 justify-center"><div class="logo"><a href="https://legalhackers.org/" target="_blank"><img src="images/logos/logo_legal_hackers.png?<?php print time(); ?>"></a></div></div>
+                </div>
 
 	</div>
 	
