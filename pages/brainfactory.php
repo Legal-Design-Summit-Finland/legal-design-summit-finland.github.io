7ed1a8f4f24438a9ff226e076c486a73f44816fc
Antti Toivonen <ast@kranken.dyndns.org>
2019-06-18 22:19:06 +0300

    Updated Brainfactory content

--- pages/brainfactory.php
+++ pages/brainfactory.php
@@ -19,19 +19,22 @@
 				
 				<div class="col-lg-8 col-lg-offset-2">
 					
-					<p>Brainfactory is an interdisciplinary hands-on workshop on legal design. No prior knowledge of legal design is required but the objective of the Brainfactory is to serve as a taster of what legal design may be in practise. During the weekend, participants will obtain practical tools on how to solve a legal problem through design. Brainfactory has two tracks: public and private track.</p>
+					<p>Brainfactory is an interdisciplinary hands-on workshop on legal design. No prior knowledge of legal design is required but the objective of the Brainfactory is to serve as a taster of what legal design may be in practise. During the intensive 3-days, participants will obtain practical tools on how to solve a legal problem through design. Brainfactory has three different tracks which will be published in August for the selected participants.</p>
 
 					<h4>Application</h4>
-					<p>The application to the Brainfactory will open in June. The participation is limited and we will inform you of admittance as soon as possible.</p>
+					<p>The application to the Brainfactory is now open. The participation is limited and we will inform you of admittance latest in the end of June, 2019. <a href="https://forms.gle/uU1my3zm8dwSETXi8" target="_blank">Apply here!</a></p>
 
 					<h4>Tracks</h4>
-					<p>The Brainfactory will have two tracks: private and public. In the private track, the legal design problem to be solved relates to private law. It is thus more of commercial and consumer law in nature. In the public track, on the other hand, the legal design problem touches upon issues relevant in public law, like the right to privacy or access to justice. When making the team formation, we try to take into account which track you have marked as preferred in your application.</p>
+					<p>This year Brainfactory will have multiple tracks built around different fields of law, like privacy, contracts and ligitations, from both public and private sector. The tracks and sponsorts behind the given challenges will be published in August 2019.</p>
 
 					<h4>Teams</h4>
-					<p>During the Brainfactory you will not only get to learn about legal design but you also get new friends! The workshop takes place in interdisciplinary teams of four people. The teams are formed on Friday.</p>
+					<p>During the Brainfactory you will not only get to learn about legal design but you also get new friends! The workshop takes place in interdisciplinary teams of four people. The teams are formed on Day 1.</p>
 
 					<h4>For whom?</h4>
-					<p>Brainfactory is meant for legal design enthusiasts with background in law, design, business or technology. Both students and professionals are welcome to apply.</p>
+					<p>Brainfactory is meant for legal design enthusiasts with a background in law, design, business or technology. Both students and professionals are welcome to apply.</p>
+
+					<h4>Something in mind?</h4>
+                                        <p>Please contact us <a href="mailto:<?php print encode_email('brainfactory@legaldesignsummit.com'); ?>"><?php print encode_email('brainfactory@legaldesignsummit.com'); ?></a> </p>
 
 				</div>
 				
@@ -50,8 +53,8 @@
 					<ul class="list justify-left">
 						<li class="large">Meet your interdisciplinary team!</li>
 						<li class="large">Opening and introduction to legal design</li>
+						<li class="large">Inspirational talks on last years Brainfactory participants - where are they now?</li>
 						<li class="large">Presenting the problems</li>
-						<li class="large">Snacks and Drinks</li>
 					</ul>
 				</div>
 				
@@ -59,20 +62,17 @@
 					<h2 class="plain">2. Day</h2>
 					<ul class="list justify-left">
 						<li class="large">Brainstorming & producing a solution to a legal design problem</li>
-						<li class="large">Expert lecture on legal design: providing the participants with tools</li>
+						<li class="large">Expert lectures</li>
 						<li class="large">Producing legal design solutions with the support of mentors</li>
-						<li class="large">Breakfast, Lunch and Dinner</li>
 					</ul>
 				</div>
 				
 				<div class="col-md-4 col-third">
 					<h2 class="plain">3. Day</h2>
 					<ul class="list justify-left">
-						<li class="large">How to transform a legal design idea into business?</li>
-						<li class="large">Expert lecture on commercialising & pitching</li>
-						<li class="large">Startup & business development coaches and venture capitalists present to support the participants to prepare to pitch their work</li>
+						<li class="large">Finalizing the outcomes</li>
+						<li class="large">Expert lectures</li>
 						<li class="large">Pitching to the Brainfactory Board</li>
-						<li class="large">Breakfast, Lunch and Drinks</li>
 					</ul>
 				</div>
 				
