c4a67b6024e073fe3d54242d32a09718a2b6519a
Antti Toivonen <ast@kranken.dyndns.org>
2019-07-05 23:54:27 +0300

    Added SDN and Merilampi logos

--- regions/content-logos.php
+++ regions/content-logos.php
@@ -4,8 +4,9 @@
                <h3>Made possible by</h3>
 
                <div class="row padding-top-20">
-			<div class="col-md-2 col-md-offset-4 justify-center"><div class="logo"><a href="https://www.hel.fi" target="_blank"><img src="images/logos/logo_helsinki.jpg?<?php print time(); ?>"></a></div></div>
+			<div class="col-md-2 col-md-offset-2 justify-center"><div class="logo"><a href="https://www.hel.fi" target="_blank"><img src="images/logos/logo_helsinki.jpg?<?php print time(); ?>"></a></div></div>
 			<div class="col-md-2 justify-center"><div class="logo"><a href="http://jandersdean.com/" target="_blank"><img src="images/logos/logo_janders_dean.png?<?php print time(); ?>"></a></div></div>
+			<div class="col-md-2 justify-center"><div class="logo"><a href="https://www.merilampi.com/" target="_blank"><img src="images/logos/logo_merilampi.jpg?<?php print time(); ?>"></a></div></div>
                </div>
 
        </div>
@@ -30,8 +31,9 @@
 			<div class="col-md-2 justify-center"><div class="logo"><a href="https://www.ml-tech.org" target="_blank"><img src="images/logos/logo_mltech.jpg?<?php print time(); ?>"></a></div></div>
 		</div>
                 <div class="row">
-			<div class="col-md-2 col-md-offset-4 justify-center"><div class="logo"><a href="https://europe-legaltech.org" target="_blank"><img src="images/logos/logo_elta.png?<?php print time(); ?>"></a></div></div>
-                        <div class="col-md-2 justify-center"><div class="logo"><a href="https://legalhackers.org/" target="_blank"><img src="images/logos/logo_legal_hackers.png?<?php print time(); ?>"></a></div></div>
+			<div class="col-md-2 col-md-offset-2 justify-center"><div class="logo"><a href="https://europe-legaltech.org" target="_blank"><img src="images/logos/logo_elta.png?<?php print time(); ?>"></a></div></div>
+			<div class="col-md-2 justify-center"><div class="logo"><a href="https://legalhackers.org/" target="_blank"><img src="images/logos/logo_legal_hackers.png?<?php print time(); ?>"></a></div></div>
+			<div class="col-md-2 justify-center"><div class="logo"><a href="https://www.service-design-network.org/chapters/finland" target="_blank"><img src="images/logos/logo_sdn.png?<?php print time(); ?>"></a></div></div>
                 </div>
 
 	</div>
