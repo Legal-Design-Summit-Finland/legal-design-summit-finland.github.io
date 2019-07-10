2da97b4306c987eab08fd78bae3fe9913a5a2470
Antti Toivonen <ast@kranken.dyndns.org>
2019-07-10 22:27:24 +0300

    Added Edita logo

--- regions/content-logos.php
+++ regions/content-logos.php
@@ -7,6 +7,9 @@
 			<div class="col-md-4 justify-center"><div class="logo"><a href="https://www.hel.fi" target="_blank"><img src="images/logos/logo_helsinki.jpg?<?php print time(); ?>"></a></div></div>
 			<div class="col-md-4 justify-center"><div class="logo"><a href="http://jandersdean.com/" target="_blank"><img src="images/logos/logo_janders_dean.png?<?php print time(); ?>"></a></div></div>
 			<div class="col-md-4 justify-center"><div class="logo"><a href="https://www.merilampi.com/" target="_blank"><img src="images/logos/logo_merilampi.jpg?<?php print time(); ?>"></a></div></div>
+		</div>
+		<div class="row padding-top-20">
+			<div class="col-md-4 col-md-offset-4 justify-center"><div class="logo"><a href="https://www.editapublishing.fi/" target="_blank"><img src="images/logos/logo_edita.png?<?php print time(); ?>"></a></div></div>
                </div>
 
        </div>
