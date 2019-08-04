c16fba030e5d5cb8598c87a4bb460057edc16371
Antti Toivonen <ast@kranken.dyndns.org>
2019-08-04 23:10:44 +0300

    Added program for 2019

--- pages/program.php
+++ pages/program.php
@@ -6,26 +6,44 @@
 
 		<div class="justify-center">
 
-			<h2>The Legal Design Summit Program</h2>
+			<h2>The Legal Design Summit Program 2019</h2>
 
 		</div>
 
-		<div class="justify-center">
+		<div class="justify-left">
 
 			<div id="program">
 
-				<h4>Coming soon!</h4>
+				<h4>10:00 Welcome Words</h4>
 
-				<h4>The main themes are:</h4>
-				<p>1. Legal Design: from thinking to doing</p>
-				<p>2. Transforming Strategies (into a culture?)</p>
-				<p>3. Impact and Beyond</p>
-
-				<p><img src="images/events/call_for_speakers.png?<?php print time(); ?>"></p>
-
-				<p>Legal Design Summit is always looking for the "what's next?" of legal design. Apply now!</p>
-
-				<p>Check out our <a href="/2017#section-program">2017</a> and <a href="/2016#section-program">2016</a> program.</p>
+				<h4>1. Legal Design from thinking to doing</h4>
+				<blockquote>
+					<p>10:10 – 10:40 <strong>What is Legal Design?</strong><br><span class="small"><strong>Dan Jackson</strong> (Executive Director, NuLawLab, Northwestern University School of Law)</span></p>
+                                        <p>10:40 – 11:00 <strong>10 Reasons Why NOT to do Legal Design</strong><br><span class="small"><strong>Marco Imperiale</strong> (Lawyer and Innovation Officer at LCA, Teacher at Harward Law)</span></p>
+                                        <p>11:00 – 11:30  <strong>Strategic Blueprints for Legal Design and Access to Justice</strong><br><span class="small"><strong>Margaret Hagan</strong> (Director, Legal Design Lab, Stanford Law)</span></p>
+                                        <p>11:30 – 12:00 <strong>Experiences from Legal Design in Courts and Dispute Resolution</strong><br><span class="small"><strong>Stacy Butler</strong> (Director, Innovation for Justice, University of Arizona)</span><br><span class="small"><strong>Andrea Lindblom</strong> (Administrative Chief, Helsingborgs Tingsrätt)</span><br><span class="small"><strong>Ylva Norling Jönsson</strong> (Chief Judge, Helsingborg Tingsrätt)</span><br><span class="small"><strong>Santiago Pardo Rodriguez</strong> (Professor of the Legal Design Lab of the University of Andes & Law Clerk at the Colombian Peace Tribunal)</span></p>
+				</blockquote>
+				<h4>12:00 – 13:00  Lunch</h4>
+				<h4>2. Transforming Strategies (into a culture)</h4>
+				<blockquote>
+					<p>13:00 – 13:20 <strong>What Does Transforming Strategies into a Culture Mean?</strong><br><span class="small"><strong>Esko Kilpi</strong> (Researcher, Author and Lecturer on post-industrial work)</span></p>
+					<p>13:20 – 13:40 <strong>Interaction Between Legal and Design Cultures and Mindsets</strong><br><span class="small"><strong>Michael Doherty</strong> (Principal Law Lecturer at University of Central Lancashire)</span></p>
+					<p>13:40 – 14:10 <strong>Transforming Strategies and Culture in Law Firms</strong><br><span class="small"><strong>Alex Smith</strong> (Global Product Lead, iManage)</span><br><span class="small"><strong>Laura Fauqueur</strong> (Marketing and BD Manager, Adarve Abogados)</span><br><span class="small"><strong>Tom Connor</strong> (Innovation Manager, Matheson)</span></p>
+					<p>14:10 – 14:30 <strong>Case: Legal Design in the Finance Sector</strong><br><span class="small"><strong>Marie Potel-Saville</strong> (Founder and CEO, Amurabi)</span><br><span class="small"><strong>Elisabeth Talbourdet</strong> (Project Manager, Amurabi)</span></p>
+					<p>14:30 – 15:00  <strong>Strategic Implementation of Legal Technology</strong><br><span class="small"><strong>Jack Pineda Dale</strong> (Legal Director, Microsoft Central and EasternEurope)</span><br><span class="small"><strong>Matthias Strand</strong> (Head of Legal, Microsoft Western Europe)</span></p>
+<p>15:00 – 15:20 <strong>World News of Legal Design</strong><br><span class="small"><strong>Meera Klemola</strong> (Founder and Director of Strategy, Observ)</span><br><span class="small"><strong>Emma Hertzberg</strong> (Founder and Creative Director, Observ)</span></p>
+				</blockquote>
+				<h4>15:20 – 16:00 Coffee Break</h4>
+				<h4>3. Legal Design Impact - What's Next?</h4>
+				<blockquote>
+					<p>16:00 – 16:20 <strong>Leading Innovation to DESIGN Law’s Future</strong><br><span class="small"><strong>Caitlin Moon</strong> (Director of Innovation Design, Vanderbilt Law School and Director of the PoLI Institute)</span></p>
+					<p>16:20 – 16:40 <strong>Legal Design Black Box – When Something Doesn’t Work as Planned </strong><br><span class="small"><strong>Jonathan Patterson</strong> (Managing Director, DWF Ventures)</span><br><span class="small"><strong>Jason Dunning</strong> (Senior Manager, DWF Ventures)</span</p>
+					<p>16:40 – 17:00 <strong>Next Generation Strategic Management for Professional Services</strong><br><span class="small"><strong>Sebastian Hartmann</strong> (Global Head of Technology Strategy, KPMG)</span></p>
+					<p>17:00 – 17:20 <strong>Future of Legal Tech Startups</strong><br><span class="small"><strong>Maya Markovich</strong> (Head of Product, Nextlaw Labs)</span></p>
+					<p>17:20 – 17:40 <strong>Visualizing Social Justice: How Legal Design Can Build Community Power</strong><br><span class="small"><strong>Hallie Jay Pope</strong> (President, Graphic Advocacy Project)</span></p>
+					<p>17:40 – 18:10  <strong>Designing Privacy in the Digital Age: Gaming, Social Media and Children</strong><br><span class="small"><strong>Leena Kuusniemi</strong> (Legal Advisor, Leegal Oy)</span><br><span class="small"><strong>Cosimo Monda</strong> (Director, European Centre on Data Privacy and Cybersecurity)</span><br><span class="small"><strong>Heikki Tolvanen</strong> (Co-Founder & Chief Legal Engineer, PrivacyAnt)</span></p>
+					<p>18:10 – 18:30 <strong>Final Keynote: We Need to Break Out of the Echo Box – So What’s Next?</strong><br><span class="small"><strong>Matthias Dobbelaere-Welvaert</strong> (Chief Creative, Ethel)</span></p>
+				</blockquote>
 
 			</div>
 
@@ -52,10 +70,8 @@
 			<div class="speakers">
 
 				<h2>Speakers</h2>
-					<h4>To be announced soon!</h4>
+					<h4>Details to be updated soon!</h4>
 					
-					<p>Check out our <a href="/2017#section-speakers">2017</a> and <a href="/2016#section-speakers">2016</a> speakers.</p>
-
 			</div>
 
 		</div>
