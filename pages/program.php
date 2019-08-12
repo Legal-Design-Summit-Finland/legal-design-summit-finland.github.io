f628a8056e8603faa7070a59f85c0fab8bd545e0
Antti Toivonen <ast@kranken.dyndns.org>
2019-08-12 22:24:32 +0300

    Adjustments to program

--- pages/program.php
+++ pages/program.php
@@ -18,7 +18,7 @@
 
 				<h4>1. Legal Design from thinking to doing</h4>
 				<blockquote>
-					<p>10:10 – 10:40 <strong>What is Legal Design?</strong><br><span class="small"><strong>Dan Jackson</strong> (Executive Director, NuLawLab, Northeastern University School of Law)</span></p>
+					<p>10:10 – 10:40 <strong>Opening Keynote: The Path of Legal Design</strong><br><span class="small"><strong>Dan Jackson</strong> (Executive Director, NuLawLab, Northeastern University School of Law)</span></p>
                                         <p>10:40 – 11:00 <strong>10 Reasons Why NOT to do Legal Design</strong><br><span class="small"><strong>Marco Imperiale</strong> (Lawyer and Innovation Officer at LCA, Teacher at Harward Law)</span></p>
                                         <p>11:00 – 11:30  <strong>Strategic Blueprints for Legal Design and Access to Justice</strong><br><span class="small"><strong>Margaret Hagan</strong> (Director, Legal Design Lab, Stanford Law)</span></p>
                                         <p>11:30 – 12:00 <strong>Experiences from Legal Design in Courts and Dispute Resolution</strong><br><span class="small"><strong>Stacy Butler</strong> (Director, Innovation for Justice, University of Arizona)</span><br><span class="small"><strong>Andrea Lindblom</strong> (Administrative Chief, Helsingborg District Court)</span><br><span class="small"><strong>Ylva Norling Jönsson</strong> (Chief Judge, Helsingborg District Court)</span><br><span class="small"><strong>Santiago Pardo Rodriguez</strong> (Professor of the Legal Design Lab of the University of Andes & Law Clerk at the Colombian Peace Tribunal)</span></p>
@@ -28,8 +28,8 @@
 				<blockquote>
 					<p>13:00 – 13:20 <strong>What Does Transforming Strategies into a Culture Mean?</strong><br><span class="small"><strong>Esko Kilpi</strong> (Researcher, Author and Lecturer on post-industrial work)</span></p>
 					<p>13:20 – 13:40 <strong>Legal Culture vs Design Culture: Tensions and Opportunities</strong><br><span class="small"><strong>Michael Doherty</strong> (Principal Law Lecturer at University of Central Lancashire)</span></p>
-					<p>13:40 – 14:10 <strong>Transforming Strategies and Culture in Law Firms</strong><br><span class="small"><strong>Alex Smith</strong> (Global Product Lead, iManage)</span><br><span class="small"><strong>Laura Fauqueur</strong> (Marketing and BD Manager, Adarve Abogados)</span><br><span class="small"><strong>Tom Connor</strong> (Innovation Manager, Matheson)</span></p>
-					<p>14:10 – 14:30 <strong>Case: Legal Design in the Finance Sector</strong><br><span class="small"><strong>Marie Potel-Saville</strong> (Founder and CEO, Amurabi)</span><br><span class="small"><strong>Elisabeth Talbourdet</strong> (Project Manager, Amurabi)</span></p>
+					<p>13:40 – 14:10 <strong>Transforming Strategies and Culture in Law Firms</strong><br><span class="small"><strong>Alex Smith</strong> (Global Product Lead, iManage)</span><br><span class="small"><strong>Laura Fauqueur</strong> (Marketing and BD Manager, Adarve Abogados)</span><br><span class="small"><strong>Tom Connor</strong> (Innovation Project Manager, Matheson)</span></p>
+					<p>14:10 – 14:30 <strong>Legal Design for Real-Life Compliance: Anti-Money Laundering, Anti-Financing of Terrorism and Anti-Corruption</strong><br><span class="small"><strong>Marie Potel-Saville</strong> (Founder and CEO, Amurabi)</span><br><span class="small"><strong>Elisabeth Talbourdet</strong> (Project Manager, Amurabi)</span></p>
 					<p>14:30 – 15:00  <strong>Strategic Implementation of Legal Technology</strong><br><span class="small"><strong>Jack Pineda Dale</strong> (Legal Director, Microsoft Central and EasternEurope)</span><br><span class="small"><strong>Matthias Strand</strong> (Head of Legal, Microsoft Western Europe)</span></p>
 <p>15:00 – 15:20 <strong>World News of Legal Design</strong><br><span class="small"><strong>Meera Klemola</strong> (Founder & Director of Strategy, Observ)</span><br><span class="small"><strong>Emma Hertzberg</strong> (Founder & Creative Director, Observ)</span></p>
 				</blockquote>
@@ -37,7 +37,7 @@
 				<h4>3. Legal Design Impact - What's Next?</h4>
 				<blockquote>
 					<p>16:00 – 16:20 <strong>Leading Innovation to DESIGN Law’s Future</strong><br><span class="small"><strong>Caitlin Moon</strong> (Director of Innovation Design, Vanderbilt Law School and Director of the PoLI Institute)</span></p>
-					<p>16:20 – 16:40 <strong>Legal Design Black Box – When Something Doesn’t Work as Planned </strong><br><span class="small"><strong>Jonathan Patterson</strong> (Managing Director, DWF Ventures)</span><br><span class="small"><strong>Jason Dunning</strong> (Senior Manager, DWF Ventures)</span</p>
+					<p>16:20 – 16:40 <strong>Legal Design Black Box – When Something Doesn’t Work as Planned </strong><br><span class="small"><strong>Jason Dunning</strong> (Senior Manager, DWF Ventures)</span</p>
 					<p>16:40 – 17:00 <strong>Next Generation Strategic Management for Professional Services</strong><br><span class="small"><strong>Sebastian Hartmann</strong> (Global Head of Technology Strategy, KPMG)</span></p>
 					<p>17:00 – 17:20 <strong>Future of Legal Tech Startups</strong><br><span class="small"><strong>Maya Markovich</strong> (Head of Product, Nextlaw Labs)</span></p>
 					<p>17:20 – 17:40 <strong>Visualizing Social Justice: How Legal Design Can Build Community Power</strong><br><span class="small"><strong>Hallie Jay Pope</strong> (President, Graphic Advocacy Project)</span></p>
