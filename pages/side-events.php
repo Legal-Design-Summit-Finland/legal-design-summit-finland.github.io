1a13ab07b65c5de16166578fb54ebca1f78e3ee8
Antti Toivonen <ast@kranken.dyndns.org>
2019-08-12 23:06:36 +0300

    Added side events

--- pages/side-events.php
+++ pages/side-events.php
@@ -7,11 +7,43 @@
         <div id="content-side-events" class="justify-center">
 
 		<h2>Side Events</h2>
-		<h4>11-12.9.2019</h4>
-		<p>The side events are related events organized by our partners. They are a unique chance to learn more about more specific legal design subject, hear case examples and to network.</p>
-		<p>The side events will be announced and posted later and you can register to the side events through the links on on this page.</p>
-		<p><strong>Are you interested to organize a side event? Contact us!</strong> <a href="mailto:&#105;&#110;&#102;&#111;&#064;&#108;&#101;&#103;&#097;&#108;&#100;&#101;&#115;&#105;&#103;&#110;&#115;&#117;&#109;&#109;&#105;&#116;&#046;&#099;&#111;&#109;">&#105;&#110;&#102;&#111;&#064;&#108;&#101;&#103;&#097;&#108;&#100;&#101;&#115;&#105;&#103;&#110;&#115;&#117;&#109;&#109;&#105;&#116;&#046;&#099;&#111;&#109;</a></p>
-	</div>
+	
+		<div class="row">
+
+			<div class="col-md-6">
+
+				<h3>SIDE EVENT BY Castrén & Snellman:<br><span class="small">Session on regulation & ethics of AI</span></h3>
+					<img src="images/logos/logo_castrensnellman.png">
+					<div class="justify-left">
+                                                <p><strong>DATE AND TIME:</strong> Thursday 12.9.2019 at 16:00 - 18:00</p>
+                                                <p><strong>LOCATION:</strong> <a href="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d1984.8314524442026!2d24.94507115194563!3d60.16696908187111!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x46920bcecb53b045%3A0xd80411aa06b8ccbe!2sEtel%C3%A4esplanadi+14%2C+00130+Helsinki!5e0!3m2!1sen!2sfi!4v1565639501999!5m2!1sen!2sfi" class="lightbox">Eteläesplanadi 14, 6th Floor</a></p>
+                                                <p><strong>SIGN UP:</strong> <a href="http://email.castren.fi/public/event/RegistrationForm/424058417444465D4A7840">http://email.castren.fi/public/event/RegistrationForm/424058417444465D4A7840</a></p>
+                                        </div>
+			       
+			</div>
+
+			<div class="col-md-6">
+
+				<div class="padding-top-sm-40"></div>
+
+				<h3>SIDE EVENT BY SisVentures:<br><span class="small">Legal Design and Sustainable Development Goals</span></h3>
+                                        <img src="images/logos/logo_sisventures.jpg">
+                                        <div class="justify-left">
+                                                <p>The 2030 Agenda for Sustainable Development was adopted at the United Nations in 2015. The 17 Sustainable Development Goals (SDGs) comprising the Agenda give the international community a shared blueprint for peace and prosperity. Over 90% of these goals and targets correspond with international human rights obligations. Yet legal design hasn’t been used as a tool for attaining them.</p>
+						<p>Sis Ventures is a social impact start-up working to empower immigrant women. We have a unique, person-first approach to entrepreneurship incubating. We use legal design and legal tech to overcome the biggest obstacle immigrant women report on becoming an entrepreneur: access to legal information and advice needed to start and run a business.</p>
+						<p>The international community has 11 years left to reach the Sustainable Development Goals. Some great progress has been made but we need to pick up the pace.</p>
+						<p>Please join us in a cocktail event the night before Legal Design Summit to learn more about Sis Ventures’ work and to further explore how legal design and legal tech could be used to advance the Agenda 2030.</p>
+						<p>The venue, Kunsthalle Helsinki, is a neo-classicist architectural gem often cited as the most elegant exhibition space in Helsinki.  Come and be inspired with us!</p>
+                                                <p><strong>DATE AND TIME:</strong> Thursday 12.9.2019 at 19:00 - 21:00</p>
+                                                <p><strong>LOCATION:</strong> <a href="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d1984.5211563772039!2d24.92914965194582!3d60.172105681873134!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x46920a330621a21b%3A0x357551754892d31c!2sKunsthalle+Helsinki!5e0!3m2!1sen!2sfi!4v1565639700958!5m2!1sen!2sfi" class="lightbox">Kunsthalle Helsinki (Taidehalli), Nervanderinkatu 3</a></p>
+                                                <p><strong>SIGN UP:</strong> <a href="http://bit.ly/3181X1p" class="lightbox">http://bit.ly/3181X1p</a></p>
+					</div>
+
+				</div>
+
+			</div>
+
+		</div>
 		
 		<!-- Content Main Section Ends -->
 		
