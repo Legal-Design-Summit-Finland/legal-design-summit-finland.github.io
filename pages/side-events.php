197861b7166d8d6b23c8cba5c55e740a113a6fbf
Antti Toivonen <ast@kranken.dyndns.org>
2019-08-15 22:12:48 +0300

    Added Fondia side event. Added description to Castren & Snellman side event.

--- pages/side-events.php
+++ pages/side-events.php
@@ -13,13 +13,13 @@
 			<div class="col-md-6">
 
 				<h3>SIDE EVENT BY Castrén & Snellman:<br><span class="small">Session on regulation & ethics of AI</span></h3>
-					<img src="images/logos/logo_castrensnellman.png">
-					<div class="justify-left">
-						<p>Stay tuned! More information about the event coming up soon!</p>
-						<p><strong>DATE AND TIME:</strong> Thursday 12.9.2019 at 16:00 - 18:00</p>
-                                                <p><strong>LOCATION:</strong> <a href="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d1984.8314524442026!2d24.94507115194563!3d60.16696908187111!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x46920bcecb53b045%3A0xd80411aa06b8ccbe!2sEtel%C3%A4esplanadi+14%2C+00130+Helsinki!5e0!3m2!1sen!2sfi!4v1565639501999!5m2!1sen!2sfi" class="lightbox">Eteläesplanadi 14, 6th Floor</a></p>
-                                                <p><strong>SIGN UP:</strong> <a href="http://email.castren.fi/public/event/RegistrationForm/424058417444465D4A7840">http://email.castren.fi/public/event/RegistrationForm/424058417444465D4A7840</a></p>
-                                        </div>
+				<img src="images/logos/logo_castrensnellman.png">
+				<div class="justify-left">
+					<p>Castrén & Snellman’s 130 years of expertise in demanding legal services have enabled us to grow into a firm of over 250 people with a mission to build new success stories. We are driven by our Spark: our enthusiasm which drives us to always do better. You are warmly welcome to our side event where futurist Kaj Sotala and our privacy expert, Partner Eija Warma, discuss regulation and ethics of AI. We hope that you will join us and share your thoughts on the topic. We believe that technology and innovation enable us to provide legal services of higher quality and add value to our clients. We are eager to hear your stories and experiences and how you have applied legal tech and legal design in your organisation. We provide seamless first-class services through our offices in Helsinki, Moscow and St Petersburg. In other parts of the world, we work with an extensive international network of law firms, which we have vetted for reliability and reputability. Seeing our clients succeed is what we strive for.</p>
+					<p><strong>DATE AND TIME:</strong> Thursday 12.9.2019 at 16:00 - 18:00</p>
+                                        <p><strong>LOCATION:</strong> <a href="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d1984.8314524442026!2d24.94507115194563!3d60.16696908187111!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x46920bcecb53b045%3A0xd80411aa06b8ccbe!2sEtel%C3%A4esplanadi+14%2C+00130+Helsinki!5e0!3m2!1sen!2sfi!4v1565639501999!5m2!1sen!2sfi" class="lightbox">Eteläesplanadi 14, 6th Floor</a></p>
+                                        <p><strong>SIGN UP:</strong> <a href="http://email.castren.fi/public/event/RegistrationForm/424058417444465D4A7840">http://email.castren.fi/public/event/RegistrationForm/424058417444465D4A7840</a></p>
+                                 </div>
 			       
 			</div>
 
@@ -28,20 +28,34 @@
 				<div class="padding-top-sm-40"></div>
 
 				<h3>SIDE EVENT BY SisVentures:<br><span class="small">Legal Design and Sustainable Development Goals</span></h3>
-                                        <img src="images/logos/logo_sisventures.jpg">
-                                        <div class="justify-left">
-                                                <p>The 2030 Agenda for Sustainable Development was adopted at the United Nations in 2015. The 17 Sustainable Development Goals (SDGs) comprising the Agenda give the international community a shared blueprint for peace and prosperity. Over 90% of these goals and targets correspond with international human rights obligations. Yet legal design hasn’t been used as a tool for attaining them.</p>
-						<p>Sis Ventures is a social impact start-up working to empower immigrant women. We have a unique, person-first approach to entrepreneurship incubating. We use legal design and legal tech to overcome the biggest obstacle immigrant women report on becoming an entrepreneur: access to legal information and advice needed to start and run a business.</p>
-						<p>The international community has 11 years left to reach the Sustainable Development Goals. Some great progress has been made but we need to pick up the pace.</p>
-						<p>Please join us in a cocktail event the night before Legal Design Summit to learn more about Sis Ventures’ work and to further explore how legal design and legal tech could be used to advance the Agenda 2030.</p>
-						<p>The venue, Kunsthalle Helsinki, is a neo-classicist architectural gem often cited as the most elegant exhibition space in Helsinki.  Come and be inspired with us!</p>
-                                                <p><strong>DATE AND TIME:</strong> Thursday 12.9.2019 at 19:00 - 21:00</p>
-                                                <p><strong>LOCATION:</strong> <a href="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d1984.5211563772039!2d24.92914965194582!3d60.172105681873134!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x46920a330621a21b%3A0x357551754892d31c!2sKunsthalle+Helsinki!5e0!3m2!1sen!2sfi!4v1565639700958!5m2!1sen!2sfi" class="lightbox">Kunsthalle Helsinki (Taidehalli), Nervanderinkatu 3</a></p>
-                                                <p><strong>SIGN UP:</strong> <a href="http://bit.ly/3181X1p" class="lightbox">http://bit.ly/3181X1p</a></p>
-					</div>
-
+                                <img src="images/logos/logo_sisventures.jpg">
+                                <div class="justify-left">
+                                        <p>The 2030 Agenda for Sustainable Development was adopted at the United Nations in 2015. The 17 Sustainable Development Goals (SDGs) comprising the Agenda give the international community a shared blueprint for peace and prosperity. Over 90% of these goals and targets correspond with international human rights obligations. Yet legal design hasn’t been used as a tool for attaining them.</p>
+					<p>Sis Ventures is a social impact start-up working to empower immigrant women. We have a unique, person-first approach to entrepreneurship incubating. We use legal design and legal tech to overcome the biggest obstacle immigrant women report on becoming an entrepreneur: access to legal information and advice needed to start and run a business.</p>
+					<p>The international community has 11 years left to reach the Sustainable Development Goals. Some great progress has been made but we need to pick up the pace.</p>
+					<p>Please join us in a cocktail event the night before Legal Design Summit to learn more about Sis Ventures’ work and to further explore how legal design and legal tech could be used to advance the Agenda 2030.</p>
+					<p>The venue, Kunsthalle Helsinki, is a neo-classicist architectural gem often cited as the most elegant exhibition space in Helsinki.  Come and be inspired with us!</p>
+                                        <p><strong>DATE AND TIME:</strong> Thursday 12.9.2019 at 19:00 - 21:00</p>
+                                        <p><strong>LOCATION:</strong> <a href="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d1984.5211563772039!2d24.92914965194582!3d60.172105681873134!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x46920a330621a21b%3A0x357551754892d31c!2sKunsthalle+Helsinki!5e0!3m2!1sen!2sfi!4v1565639700958!5m2!1sen!2sfi" class="lightbox">Kunsthalle Helsinki (Taidehalli), Nervanderinkatu 3</a></p>
+                                        <p><strong>SIGN UP:</strong> <a href="http://bit.ly/3181X1p" class="lightbox">http://bit.ly/3181X1p</a></p>
 				</div>
 
+			</div>
+
+		</div>
+
+		<div class="row">
+
+			<div class="col-md-6">
+				<div class="padding-top-sm-40"></div>
+
+                                <h3>SIDE EVENT BY Fondia:<br><span class="small">Nurturing culture in to a strategy – it’s a two-way road</span></h3>
+                                <img src="images/logos/logo_fondia.png">
+                                <div class="justify-left">
+                                        <p>Organisational culture can act as a beacon, which guides the organisation in the right direction even when fog obscures visibility of the future. Is your organisation’s lighthouse in working condition and ready to lead you towards new waters? How a culture changes and what is emphasized about it are things that can be directed if regular attention is paid to this. It is unlikely that a culture can ever be changed completely, but by highlighting the sought-after and desired characteristics of the culture the organisation learns new habits and slowly the old ways are forgotten. Just as humans can learn a new way to think, so can organisations. The legal service company Fondia Plc has been chosen as one of Europe’s most innovative legal companies. As part of its ongoing efforts to develop its business, Fondia conducted a survey last summer on how legal services are purchased. More than 100 CEOs responded to the survey. The results revealed that there is demand, for innovative and transparent law firms. Fondia’s continuous legal service operating model LDaaS combined with MyFondia’s digital operating environment provides customers with easy-to-predict costs, smoother communication and improved legal service resource management. Come and hear the essence of the Fondia’s core business offering and how the company’s unique culture plays a significant role in the business development.</p>
+                                        <p><strong>DATE AND TIME:</strong> Thursday 12.9.2019 at 17:00 - 19:00</p>
+                                        <p><strong>LOCATION:</strong> <a href="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d1984.8262936539838!2d24.93730031669397!3d60.16705448195979!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x46920bcb93be0cad%3A0x3573400585621a1e!2sFondia+Oyj!5e0!3m2!1sen!2sfi!4v1565896023154!5m2!1sen!2sfi" class="lightbox">Lönnrotinkatu 5, 4th floor, Helsinki</a></p>
+                                        <p><strong>SIGN UP:</strong> <a href="https://www.lyyti.in/Legal_design_summit_side_event_FondiaOyj" class="lightbox">https://www.lyyti.in/Legal_design_summit_side_event_FondiaOyj</a></p>
 			</div>
 
 		</div>
