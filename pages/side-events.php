a117aa823d5b9e9316871f25797afd22eaf3b3bb
Antti Toivonen <ast@kranken.dyndns.org>
2019-08-22 00:00:46 +0300

    Added Legal Hackers side event. Fixed typo's in Fondia side event.

--- pages/side-events.php
+++ pages/side-events.php
@@ -49,21 +49,43 @@
 		<div class="row">
 
 			<div class="col-md-6">
-				<div class="padding-top-sm-40"></div>
 
-                                <h3>SIDE EVENT BY Fondia:<br><span class="small">Nurturing culture in to a strategy – it’s a two-way road</span></h3>
+                                <h3>SIDE EVENT BY Fondia:<br><span class="small">Case Fondia: Nurturing culture into a strategy – it’s a two-way road</span></h3>
                                 <img src="images/logos/logo_fondia.png">
                                 <div class="justify-left">
 					<p>Organisational culture can act as a beacon, which guides the organisation in the right direction even when fog obscures visibility of the future. Is your organisation’s lighthouse in working condition and ready to lead you towards new waters? How a culture changes and what is emphasized about it are things that can be directed if regular attention is paid to this. It is unlikely that a culture can ever be changed completely, but by highlighting the sought-after and desired characteristics of the culture the organisation learns new habits and slowly the old ways are forgotten. Just as humans can learn a new way to think, so can organisations.</p>
-					<p>The legal service company Fondia Plc has been chosen as one of Europe’s most innovative legal companies. As part of its ongoing efforts to develop its business, Fondia conducted a survey last summer on how legal services are purchased. More than 100 CEOs responded to the survey. The results revealed that there is demand, for innovative and transparent law firms. Fondia’s continuous legal service operating model LDaaS combined with MyFondia’s digital operating environment provides customers with easy-to-predict costs, smoother communication and improved legal service resource management.</p>
-					<p>Come and hear the essence of the Fondia’s core business offering and how the company’s unique culture plays a significant role in the business development.</p>
+					<p>The legal service company Fondia Plc has been chosen as one of Europe’s most innovative legal companies. As part of its ongoing efforts to develop its business, Fondia conducted a survey last summer on how legal services are purchased. More than 100 CEOs responded to the survey. The results revealed that there is demand for innovative and transparent law firms. Fondia’s continuous legal service operating model LDaaS combined with MyFondia’s digital operating environment provides customers with easy-to-predict costs, smoother communication and improved legal service resource management.</p>
+					<p>Come and hear the essence of Fondia’s core business offering and how the company’s unique culture plays a significant role in the business development.</p>
                                         <p><strong>DATE AND TIME:</strong> Thursday 12.9.2019 at 17:00 - 19:00</p>
                                         <p><strong>LOCATION:</strong> <a href="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d1984.8262936539838!2d24.93730031669397!3d60.16705448195979!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x46920bcb93be0cad%3A0x3573400585621a1e!2sFondia+Oyj!5e0!3m2!1sen!2sfi!4v1565896023154!5m2!1sen!2sfi" class="lightbox">Lönnrotinkatu 5, 4th floor, Helsinki</a></p>
                                         <p><strong>SIGN UP:</strong> <a href="https://www.lyyti.in/Legal_design_summit_side_event_FondiaOyj" class="lightbox">https://www.lyyti.in/Legal_design_summit_side_event_FondiaOyj</a></p>
+				</div>
 			</div>
 
+			<div class="col-md-6">
+                                <div class="padding-top-sm-40"></div>
+
+                                <h3>SIDE EVENT BY Legal Hackers:<br><span class="small">Legal (Design) Hackers Welcome Party</span></h3>
+                                <img src="images/logos/logo_legal-hackers.png">
+                                <div class="justify-left">
+					<p>Are you part of a Legal Hacker chapter somewhere on the planet? Or maybe you are just a Hacker or a Legal - or none of them, but you are passionate about legal tech, legal design and IT law? Would you like to meet legal digital innovators from all over the world?</p>
+					<p><strong>Program:</strong></p>
+					<p>8.30 p.m &nbsp;&nbsp;&nbsp;&nbsp;Doors open (free of charge of course) at second floor</p>
+					<p>8.40 p.m &nbsp;&nbsp;&nbsp;&nbsp;Helsinki Legal Hackers welcome toast</p>
+					<p>Legal Hackers is a global movement of lawyers, policymakers, designers, technologists, and academics who explore and develop creative solutions to some of the most pressing issues at the intersection of law and technology. Through local meetups, hackathons, and workshops, Legal Hackers spot issues and opportunities where technology can improve and inform the practice of law and where law, legal practice, and policy can adapt to rapidly changing technology.</p>
+					<p>8.45 p.m. &nbsp;&nbsp;&nbsp;&nbsp;Contract Mill Oy’s (our lovely sponsor) founder Kaisa Kromhof: This is how we solve customers’ problems via design   principles: Onboarding by Design</p>
+					<p>Contract Mill is an accomplished Legal Tech start-up grown to a scale-up company bringing you effective and easy to implement SaaS for Document Automation.</p>
+					<p>Mingling with like-minded people around our customized wine bar.</p>
+					<p>9.15 p.m. &nbsp;&nbsp;&nbsp;&nbsp;Introduction to the Legal Design Alliance:</p>
+					<p>LeDA is a network of lawyers, designers, technologists, academics, and other professionals who are committed to making the legal system more human-centered and effective, through the use of design. Join our open community! <a href="https://www.legaldesignalliance.org/">https://www.legaldesignalliance.org</a></p>
+					<p>Let’s warm up for the Legal Design Summit!</p>
+					<p>There will be drinks, there will be music, there will be chats from 8.30 till midnight.</p>
+                                        <p><strong>DATE AND TIME:</strong> Thursday 12.9.2019 at 20:30 - 00:00</p>
+                                        <p><strong>LOCATION:</strong> <a href="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d1984.7809698599478!2d24.93885435194565!3d60.16780478187155!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x46920bcbf5510ea1%3A0x2affe8f37912db98!2sMannerheimintie+10%2C+00101+Helsinki%2C+Finland!5e0!3m2!1sen!2sus!4v1566420448634!5m2!1sen!2sus" class="lightbox">Marski Bar, Mannerheimintie 10, Helsinki</a></p>
+                                        <p><strong>SIGN UP:</strong> <a href="http://bit.ly/LegalHackersWelcomeLDS" class="lightbox">http://bit.ly/LegalHackersWelcomeLDS</a></p>
+                        	</div>
+			</div>
 		</div>
-		
 		<!-- Content Main Section Ends -->
 		
 	</div>
