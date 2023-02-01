78cc98dd0fefc2219f3c108e1ef88bef9fe605d6
Calin Capitanu <calincapitanu@gmail.com>
2023-02-01 21:50:17 +0100

    Re-added about-us and why-attend, cleanup in pp

--- pages/about-us.php
+++ pages/about-us.php
@@ -1,141 +1,141 @@
 <div class="wrapper about-us">
 
-	<div class="container">
-	
-        <!-- Content Section Begins -->
-        
-        <div id="content-about-us">
+  <div class="container">
 
-			<div class="justify-center">
+    <!-- Content Section Begins -->
 
-				<h2>About Us</h2>
+    <div id="content-about-us">
 
-				<p>We are a team of ambitious doers, who decided to create a forum for international experts in the fields of law, design, business and digital services.</p>
+      <div class="justify-center">
 
-			</div>
+	<h2>About Us</h2>
 
-		</div>
-		
-		<!-- Content Main Section Ends -->
-		
-	</div>
-	
+	<p>We are a team of ambitious doers, who decided to create a forum for international experts in the fields of law, design, business and digital services.</p>
+
+      </div>
+
+    </div>
+
+    <!-- Content Main Section Ends -->
+
+  </div>
+
 </div>
 
 
 
 <div class="wrapper color-secondary team">
 
-	<div class="container">
-		
-		<!-- Content Section Begins -->
-		
-		<div id="content-team">
+  <div class="container">
 
-			<div class="justify-center">
+    <!-- Content Section Begins -->
 
-				<h2>Our Team</h2>
-				
-				<div class="team">
-					
-					<div class="row equalize">
+    <div id="content-team">
 
-						<div class="col-xs-6 col-md-3">
-							<div class="image"><img src="images/team/pilvi-alopaeus.png"></div>
-							<p>Pilvi Alopaeus<br><span class="small">CEO<br><?php print encode_email('pilvi.alopaeus@legaldesignsummit.com', true); ?></p>
-							<p class="margin-bottom-0"><a href="https://www.linkedin.com/in/pilvialopaeus/" class="icon linkedin">LinkedIn</a></span></p>
-						</div>
+      <div class="justify-center">
 
-						<div class="col-xs-6 col-md-3">
-                                                        <div class="image"><img src="images/team/eero-talvela.png"></div>
-                                                        <p>Eero Talvela<br><span class="small">Sales<br><?php print encode_email('eero.talvela@legaldesignsummit.com', true); ?></p>
-                                                        <p class="margin-bottom-0"><a href="https://www.linkedin.com/in/talvela/" class="icon linkedin">LinkedIn</a></span></p>
-						</div>
-						<div class="col-xs-6 col-md-3">
-                                                        <div class="image"><img src="images/team/nina-toivonen.jpg"></div>
-                                                        <p>Nina Toivonen<br><span class="small">Producer<br><?php print encode_email('nina.toivonen@legaldesignsummit.com', true); ?></p>
-                                                        <p class="margin-bottom-0"><a href="https://www.linkedin.com/in/nina-toivonen-85300b20/" class="icon linkedin">LinkedIn</a></span></p>
-						</div>
-						<div class="col-xs-6 col-md-3">
-                                                        <div class="image"><img src="images/team/henna-tolvanen.jpg"></div>
-                                                        <p>Henna Tolvanen<br><span class="small">Producer<br><?php print encode_email('henna.tolvanen@legaldesignsummit.com', true); ?></p>
-                                                        <p class="margin-bottom-0"><a href="https://www.linkedin.com/in/hennatolvanen/" class="icon linkedin">LinkedIn</a></span></p>
-                                                </div>
+	<h2>Our Team</h2>
 
-					</div>
-					<div class="row equalize">
-                                                <div class="col-xs-6 col-md-3">
-                                                        <div class="image"><img src="images/team/jukka-linna.jpg"></div>
-                                                        <p>Jukka Linna<br><span class="small">Back Office</p>
-                                                        <p class="margin-bottom-0"><a href="https://www.linkedin.com/in/jukkalinna/" class="icon linkedin">LinkedIn</a></span></p>
-						</div>
-                                                <div class="col-xs-6 col-md-3">
-                                                        <div class="image"><img src="images/team/eerika-tirkkonen.jpg"></div>
-                                                        <p>Eerika Tirkkonen<br><span class="small">Partnerships<br><?php print encode_email('partnerships@legaldesignsummit.com', true); ?></p>
-						</div>
-						<div class="col-xs-6 col-md-3">
-                                                        <div class="image"><img src="images/team/peppiina-huhtala.jpg"></div>
-                                                        <p>Peppiina Huhtala<br><span class="small">Partnerships<br><?php print encode_email('partnerships@legaldesignsummit.com', true); ?></p>
-                                                        <p class="margin-bottom-0"><a href="https://www.linkedin.com/in/peppiina-huhtala-b869bb105/" class="icon linkedin">LinkedIn</a></span></p>
-						</div>
-						<div class="col-xs-6 col-md-3">
-                                                        <div class="image"><img src="images/team/susanna-aarnivuo.jpg"></div>
-							<p>Susanna Aarnivuo<br><span class="small">Partnerships<br><?php print encode_email('partnerships@legaldesignsummit.com', true); ?></p>
-							<p class="margin-bottom-0"><a href="https://www.linkedin.com/in/aarnivuo/" class="icon linkedin">LinkedIn</a></span></p>
-                                                </div>
-					</div>
-					<div class="row equalize">
-                                                <div class="col-xs-6 col-md-3">
-                                                        <div class="image"><img src="images/team/jenni-nevala.jpg"></div>
-                                                        <p>Jenni Nevala<br><span class="small">Speakers<br><?php print encode_email('speakers@legaldesignsummit.com', true); ?></p>
-						</div>
-						<div class="col-xs-6 col-md-3">
-                                                        <div class="image"><img src="images/team/sanna-niinikoski.jpg"></div>
-                                                        <p>Sanna Niinikoski<br><span class="small">Speakers<br><?php print encode_email('speakers@legaldesignsummit.com', true); ?></p>
-                                                        <p class="margin-bottom-0"><a href="https://www.linkedin.com/in/sannaniinikoski/" class="icon linkedin">LinkedIn</a></span></p>
-						</div>
-						<div class="col-xs-6 col-md-3">
-                                                        <div class="image"><img src="images/team/maria-ahmas.png"></div>
-                                                        <p>Maria Ahmas<br><span class="small">HR</p>
-                                                        <p class="margin-bottom-0"><a href="https://www.linkedin.com/in/mariaahmas/" class="icon linkedin">LinkedIn</a></span></p>
-						</div>
-						<div class="col-xs-6 col-md-3">
-                                                        <div class="image"><img src="images/team/elise-rehula.png"></div>
-                                                        <p>Elise Rehula<br><span class="small">Brainfactory</p>
-                                                        <p class="margin-bottom-0"><a href="http://www.linkedin.com/in/eliserehula" class="icon linkedin">LinkedIn</a></span></p>
-                                                </div>
-					</div>
-                                        <div class="row equalize">
-                                                <div class="col-xs-6 col-md-3">
-                                                        <div class="image"><img src="images/team/heini-heinonen.png"></div>
-                                                        <p>Heini Heinonen<br><span class="small">Brainfactory</p>
-                                                        <p class="margin-bottom-0"><a href="https://www.linkedin.com/in/heini-heinonen-4561b1a5/" class="icon linkedin">LinkedIn</a></span></p>
-                                                </div>
-                                                <div class="col-xs-6 col-md-3">
-                                                        <div class="image"><img src="images/team/marjukka-rantala.png"></div>
-                                                        <p>Marjukka Rantala<br><span class="small">Brainfactory</p>
-                                                        <p class="margin-bottom-0"><a href="https://www.linkedin.com/in/marjukkarantala/" class="icon linkedin">LinkedIn</a></span></p>
-                                                </div>
-                                        </div>
-				</div>
-				
-				<div class="small padding-top-30">
-					<p><strong>Photos by:</strong></p>
-					<p>Svante Gullichsen Photography<br><a href="http://svantegullichsen.com" target="_blank">svantegullichsen.com</a></p>
-				</div>
-				
-				<div class="small padding-top-30"></div>
-				
-				<h2>Our Volunteers</h2>
-				<p>Legal Design Summit is organized by volunteers. Are you interested to join our team as a volunteer for the events of 2019? Fantastic! We will open the volunteer application later!</p>	
-				<img src="images/team/volunteers.jpg">
-				
-			</div>
-			
-		</div>
-		
-		<!-- Content Main Section Ends -->
-		
-	</div>
-	
+	<div class="team">
+
+	  <div class="row equalize">
+
+	    <div class="col-xs-6 col-md-3">
+	      <div class="image"><img src="images/team/pilvi-alopaeus.png"></div>
+	      <p>Pilvi Alopaeus<br><span class="small">CEO<br><?php print encode_email('pilvi.alopaeus@legaldesignsummit.com', true); ?></p>
+	      <p class="margin-bottom-0"><a href="https://www.linkedin.com/in/pilvialopaeus/" class="icon linkedin">LinkedIn</a></span></p>
+</div>
+
+<div class="col-xs-6 col-md-3">
+  <div class="image"><img src="images/team/eero-talvela.png"></div>
+  <p>Eero Talvela<br><span class="small">Sales<br><?php print encode_email('eero.talvela@legaldesignsummit.com', true); ?></p>
+  <p class="margin-bottom-0"><a href="https://www.linkedin.com/in/talvela/" class="icon linkedin">LinkedIn</a></span></p>
+</div>
+<div class="col-xs-6 col-md-3">
+  <div class="image"><img src="images/team/nina-toivonen.jpg"></div>
+  <p>Nina Toivonen<br><span class="small">Producer<br><?php print encode_email('nina.toivonen@legaldesignsummit.com', true); ?></p>
+  <p class="margin-bottom-0"><a href="https://www.linkedin.com/in/nina-toivonen-85300b20/" class="icon linkedin">LinkedIn</a></span></p>
+</div>
+<div class="col-xs-6 col-md-3">
+  <div class="image"><img src="images/team/henna-tolvanen.jpg"></div>
+  <p>Henna Tolvanen<br><span class="small">Producer<br><?php print encode_email('henna.tolvanen@legaldesignsummit.com', true); ?></p>
+  <p class="margin-bottom-0"><a href="https://www.linkedin.com/in/hennatolvanen/" class="icon linkedin">LinkedIn</a></span></p>
+</div>
+
+</div>
+<div class="row equalize">
+  <div class="col-xs-6 col-md-3">
+    <div class="image"><img src="images/team/jukka-linna.jpg"></div>
+    <p>Jukka Linna<br><span class="small">Back Office</p>
+    <p class="margin-bottom-0"><a href="https://www.linkedin.com/in/jukkalinna/" class="icon linkedin">LinkedIn</a></span></p>
+</div>
+<div class="col-xs-6 col-md-3">
+  <div class="image"><img src="images/team/eerika-tirkkonen.jpg"></div>
+  <p>Eerika Tirkkonen<br><span class="small">Partnerships<br><?php print encode_email('partnerships@legaldesignsummit.com', true); ?></p>
+</div>
+<div class="col-xs-6 col-md-3">
+  <div class="image"><img src="images/team/peppiina-huhtala.jpg"></div>
+  <p>Peppiina Huhtala<br><span class="small">Partnerships<br><?php print encode_email('partnerships@legaldesignsummit.com', true); ?></p>
+  <p class="margin-bottom-0"><a href="https://www.linkedin.com/in/peppiina-huhtala-b869bb105/" class="icon linkedin">LinkedIn</a></span></p>
+</div>
+<div class="col-xs-6 col-md-3">
+  <div class="image"><img src="images/team/susanna-aarnivuo.jpg"></div>
+  <p>Susanna Aarnivuo<br><span class="small">Partnerships<br><?php print encode_email('partnerships@legaldesignsummit.com', true); ?></p>
+  <p class="margin-bottom-0"><a href="https://www.linkedin.com/in/aarnivuo/" class="icon linkedin">LinkedIn</a></span></p>
+</div>
+</div>
+<div class="row equalize">
+  <div class="col-xs-6 col-md-3">
+    <div class="image"><img src="images/team/jenni-nevala.jpg"></div>
+    <p>Jenni Nevala<br><span class="small">Speakers<br><?php print encode_email('speakers@legaldesignsummit.com', true); ?></p>
+  </div>
+  <div class="col-xs-6 col-md-3">
+    <div class="image"><img src="images/team/sanna-niinikoski.jpg"></div>
+    <p>Sanna Niinikoski<br><span class="small">Speakers<br><?php print encode_email('speakers@legaldesignsummit.com', true); ?></p>
+    <p class="margin-bottom-0"><a href="https://www.linkedin.com/in/sannaniinikoski/" class="icon linkedin">LinkedIn</a></span></p>
+</div>
+<div class="col-xs-6 col-md-3">
+  <div class="image"><img src="images/team/maria-ahmas.png"></div>
+  <p>Maria Ahmas<br><span class="small">HR</p>
+  <p class="margin-bottom-0"><a href="https://www.linkedin.com/in/mariaahmas/" class="icon linkedin">LinkedIn</a></span></p>
+</div>
+<div class="col-xs-6 col-md-3">
+  <div class="image"><img src="images/team/elise-rehula.png"></div>
+  <p>Elise Rehula<br><span class="small">Brainfactory</p>
+  <p class="margin-bottom-0"><a href="http://www.linkedin.com/in/eliserehula" class="icon linkedin">LinkedIn</a></span></p>
+</div>
+</div>
+<div class="row equalize">
+  <div class="col-xs-6 col-md-3">
+    <div class="image"><img src="images/team/heini-heinonen.png"></div>
+    <p>Heini Heinonen<br><span class="small">Brainfactory</p>
+    <p class="margin-bottom-0"><a href="https://www.linkedin.com/in/heini-heinonen-4561b1a5/" class="icon linkedin">LinkedIn</a></span></p>
+</div>
+<div class="col-xs-6 col-md-3">
+  <div class="image"><img src="images/team/marjukka-rantala.png"></div>
+  <p>Marjukka Rantala<br><span class="small">Brainfactory</p>
+  <p class="margin-bottom-0"><a href="https://www.linkedin.com/in/marjukkarantala/" class="icon linkedin">LinkedIn</a></span></p>
+</div>
+</div>
+</div>
+
+<div class="small padding-top-30">
+  <p><strong>Photos by:</strong></p>
+  <p>Svante Gullichsen Photography<br><a href="http://svantegullichsen.com" target="_blank">svantegullichsen.com</a></p>
+</div>
+
+<div class="small padding-top-30"></div>
+
+<h2>Our Volunteers</h2>
+<p>Legal Design Summit is organized by volunteers. Are you interested to join our team as a volunteer for the events of 2019? Fantastic! We will open the volunteer application later!</p>	
+<img src="images/team/volunteers.jpg">
+
+</div>
+
+</div>
+
+<!-- Content Main Section Ends -->
+
+</div>
+
 </div>
